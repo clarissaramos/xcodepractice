d300b42261465e864cc6eb3aaef68181d5260091
Clarissa Ramos <clarissaramos@protonmail.com>
2022-08-16 10:31:36 -0700

    Initial Commit

---
+++ xcodepractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xcodepractice
+//
+//  Created by Clarissa Ramos on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Testing, testing, 1, 2, 3!")
+    }
+
+
+}
+
